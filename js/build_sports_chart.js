55274d3ceef352bfb96eeac6d2d77e12b13ee273
Peter Viechnicki <pviechnicki@deloitte.com>
2016-10-01 21:45:24 -0400

    Added total score

--- js/build_sports_chart.js
+++ js/build_sports_chart.js
@@ -6,22 +6,6 @@
 /* Globals                                         */
 /*-------------------------------------------------*/
 var viz_chart = {};
-//var jsonURL = "./data/mcps_hs_sports_records_2015_2016.json";
-
-var fakeData = [
-    {
-	"School": "one",
-	"Football": .5,
-	"Baseball": .3,
-	"FARMS Rate": .2
-    },
-    {
-	"School": "two",
-	"Football": .3,
-	"Baseball": .7,    
-	"FARMS Rate": .6
-    }
-];
 
 
 /*------------------------------------------------------------*/
@@ -33,13 +17,13 @@ function buildViz(data) {
     	.data(data)
     	.type("scatter")
     	.id("School")
-    	.x("Football")
+    	.x("Total All Sports")
     	.y("FARMS Rate")
 	.ui([
 	    {
 		"method": "x",
 		"label": "Select sport",
-		"value": ["Football", "Baseball", "Softball", "Field Hockey",
+		"value": ["Total All Sports", "Football", "Baseball", "Softball", "Field Hockey",
 			  "Lacrosse (G)", "Lacrosse (B)", "Soccer (G)",
 			  "Soccer (B)", "Basketball (G)", "Basketball (B)",
 			  "Tennis (G)", "Tennis (B)"]
@@ -60,6 +44,9 @@ getJSON(jsonFilename).then(function(response) {
     }
     //Draw the commute times graph
     buildViz(schoolsData);
+
+    //Draw the map of high schools with the same data
+    buildMap(schoolsData);
 
 }
 			  );
